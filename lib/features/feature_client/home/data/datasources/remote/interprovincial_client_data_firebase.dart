228cb6e834a855342c134a9b921768a07a672057
Joyboy <kudamono200659@gmail.com>
2021-05-06 09:43:54 -0500

    ..

--- lib/features/feature_client/home/data/datasources/remote/interprovincial_client_data_firebase.dart
+++ lib/features/feature_client/home/data/datasources/remote/interprovincial_client_data_firebase.dart
@@ -10,12 +10,18 @@ class InterprovincialClientDataFirebase {
   final PushMessage pushMessage;
   InterprovincialClientDataFirebase( {@required this.firestore, @required this.pushMessage,});
 
-  Future<bool> addRequestClient({String documentId,InterprovincialRequestEntity request, @required String fcmTokenDriver,bool update}) async{
+  Future<bool> addRequestClient({String documentId,InterprovincialRequestEntity request,bool update}) async{
     try {
       DocumentReference  dr =  await firestore.collection('interprovincial_in_service').doc(documentId);
       dr.collection('requests').add(request.toFirestore);
       DocumentSnapshot  ds = await dr.get();
+      print('..................');
+      print(ds.data());
+      print('..................');
       InterprovincialLocationDriverEntity interprovincialLocationDriver = InterprovincialLocationDriverEntity.fromJson(ds.data());
+      // print('###################');
+      // print(interprovincialLocationDriver. );
+      // print('###################');
       pushMessage.sendPushMessage(
         token: interprovincialLocationDriver.fcmToken , // Token del dispositivo del chofer
         title: 'Ha recibido una nueva solicitud',
