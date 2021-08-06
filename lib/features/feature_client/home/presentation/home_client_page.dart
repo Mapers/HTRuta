25c7f948b0409995379bbd20ffb6fe743c4a5a9a
Joyboy <kudamono200659@gmail.com>
2021-08-06 18:43:12 -0500

    fix home cliemt

--- lib/features/feature_client/home/presentation/home_client_page.dart
+++ lib/features/feature_client/home/presentation/home_client_page.dart
@@ -21,53 +21,6 @@ class _HomeClientPageState extends State<HomeClientPage> {
   
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return SideMenu(
-      key: _sideMenuKey,
-      background: primaryColor,
-      menu: MenuScreens(activeScreenName: _screenName),
-      type: SideMenuType.slideNRotate, // check above images
-      child: Scaffold(
-        key: _scaffoldKey,
-        drawer: MenuScreens(activeScreenName: _screenName),
-        body: Stack(
-          alignment: Alignment.center,
-          children: [
-            BlocBuilder<ClientServiceBloc, ClientServiceState>(
-              builder: (ctx, state){
-                DataClientServiceState data = state;
-                switch (data.typeService) {
-                  case TypeServiceEnum.taxi:
-                    return HomeScreens(parentScaffoldKey: _scaffoldKey);
-                  case TypeServiceEnum.interprovincial:
-                    return InterprovincialClientScreen(rejected: widget.rejected,);
-                  default:
-                    return Text('Service not found!');
-                }
-              },
-            ),
-            Positioned(
-              top: 50,
-              left: 10,
-              child: Container(
-                height: 40.0,
-                width: 40.0,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(100.0),),
-                ),
-                child: IconButton(
-                  icon: Icon(Icons.menu,size: 20.0,color: blackColor),
-                  onPressed: (){
-                    final _state = _sideMenuKey.currentState;
-                    if (_state.isOpened)
-                      _state.closeSideMenu(); // close side menu
-                    else
-                      _state.openSideMenu();
-                    }
-                ),
-              )
-=======
     return Scaffold(
       key: _scaffoldKey,
       drawer: MenuScreens(activeScreenName: _screenName),
@@ -103,7 +56,6 @@ class _HomeClientPageState extends State<HomeClientPage> {
                   _scaffoldKey.currentState.openDrawer();
                   }
               ),
->>>>>>> f9f9b85b80da820140bebe265d6e80790bee3433
             )
           )
           // MenuButtonWidget(parentScaffoldKey: _scaffoldKey),
